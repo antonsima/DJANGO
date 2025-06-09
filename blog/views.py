ba10a04f5875ac9cf3f74dcd3dedeca81a073a09
Anton <anton_sima@mail.ru>
2025-06-09 21:50:41 +0500

    Выполнены 3 задания из домашки

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,26 @@
-from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse, reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
 
 from blog.models import BlogPost
 
 
 class BlogPostCreateView(CreateView):
     model = BlogPost
-    template_name = 'blog/blogpost_form.html'
-    success_url = reverse_lazy('blog:blogpost_list')
-    fields = ['title', 'content', 'preview_image', 'is_published']
+    template_name = "blog/blogpost_form.html"
+    success_url = reverse_lazy("blog:blogpost_list")
+    fields = ["title", "content", "preview_image", "is_published"]
 
 
 class BlogPostListView(ListView):
     model = BlogPost
-    template_name = 'blog/blogpost_list.html'
-    context_object_name = 'posts'
+    template_name = "blog/blogpost_list.html"
+    context_object_name = "posts"
 
     def get_queryset(self):
         return BlogPost.objects.filter(is_published=True)
@@ -22,8 +28,8 @@ class BlogPostListView(ListView):
 
 class BlogPostDetailView(DetailView):
     model = BlogPost
-    template_name = 'blog/blogpost_detail.html'
-    context_object_name = 'post'
+    template_name = "blog/blogpost_detail.html"
+    context_object_name = "post"
 
     def get_object(self, queryset=None):
         obj = super().get_object(queryset)
@@ -34,12 +40,14 @@ class BlogPostDetailView(DetailView):
 
 class BlogPostUpdateView(UpdateView):
     model = BlogPost
-    template_name = 'blog/blogpost_form.html'
-    success_url = reverse_lazy('blog:blogpost_list')
-    fields = ['title', 'content', 'preview_image', 'is_published']
+    template_name = "blog/blogpost_form.html"
+    fields = ["title", "content", "preview_image", "is_published"]
+
+    def get_success_url(self):
+        return reverse("blog:blogpost_detail", kwargs={"pk": self.object.pk})
 
 
 class BlogPostDeleteView(DeleteView):
     model = BlogPost
-    template_name = 'blog/blogpost_confirm_delete.html'
-    success_url = reverse_lazy('blog:blogpost_list')
+    template_name = "blog/blogpost_confirm_delete.html"
+    success_url = reverse_lazy("blog:blogpost_list")
--- config/urls.py
+++ config/urls.py
@@ -23,7 +23,7 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("catalog/", include("catalog.urls", namespace="catalog")),
-    path('blog/', include('blog.urls', namespace="blog")),
+    path("blog/", include("blog.urls", namespace="blog")),
 ]
 
 if settings.DEBUG:
